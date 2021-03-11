a408b6707e3cfdda0a24c8038bdb7a385a0673eb
Leon Puchinger <lepu1590@gmail.com>
2021-03-11 16:18:38 +0100

    get_it setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pacemap/data/services/database.dart';
 import 'package:pacemap/widgets/addtrack.dart';
 import 'package:pacemap/widgets/tracklist.dart';
 
-void main() => runApp(PaceMapApp());
+void main() {
+  GetIt.I.registerLazySingletonAsync<DatabaseHandler>(
+    () => DatabaseHandler.initDatabase(),
+    dispose: (db) => db.dispose(),
+  );
+  runApp(PaceMapApp());
+}
 
 class PaceMapApp extends StatelessWidget {
   @override
@@ -14,6 +22,18 @@ class PaceMapApp extends StatelessWidget {
       routes: {
         "list": (_) => Tracklist(),
         "list/add": (_) => AddTrack(),
+      },
+      builder: (_, route) {
+        return FutureBuilder(
+          future: GetIt.I.allReady(),
+          builder: (_, readySnapshot) {
+            if (readySnapshot.hasData) {
+              return route!;
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        );
       },
     );
   }
